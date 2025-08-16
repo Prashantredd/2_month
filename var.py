0af9fac7eaf0ff0b9f8d59adfbbf13fcd749d5b7
prashant <gajjalaprashantreddy@gmail.com>
2025-08-16 23:04:55 +0530

    updated var.py with newline

--- var.py
+++ var.py
@@ -1,1 +1,27 @@
-print("hello")
+a="hello"
+b=43
+c=40.8
+bool=True
+print(a,b,c,bool)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(bool))
+a=10
+b=20
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b)
+print(a%b)
+s="hello world!"
+print(s[::])
+print(s[1:])
+print(s[::-1])
+print(len(s))
+print(s.upper())
+print(s.lower())
+print(s.capitalize())
+print(s.title())
+print(s.split(" "))
